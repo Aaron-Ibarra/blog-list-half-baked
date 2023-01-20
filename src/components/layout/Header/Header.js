942319cb8e9b5b7672a3eb5c94762757fc01b587
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-19 17:42:40 -0800

    header test should pass

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import './Header.css';
 export default function Header() {
-  return <header>Look at all the countries :O</header>;
+  return <header>Alchemedium</header>;
 }
